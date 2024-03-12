da815fafe8869284e5db09d1e7db78ed30d940bb
kristinabobako <kristinabobako@mail.ru>
2024-03-12 19:16:42 +0300

    POST-запрос

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
 
-  // Здесь твой код
+const userName = document.querySelector('#name');
+const userSecondName = document.querySelector('#secondName');
+const userEmail = document.querySelector('#email');
+const userPhone = document.querySelector('#phone');
+const dataProcessing = document.querySelector('#agree');
+
+const sendBtn = document.querySelector('#button');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendRequest();
+  form.reset();
 });
+
+function sendRequest() {
+  fetch('https://polinashneider.space/user', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer: kristinabobako'
+    },
+    body: JSON.stringify({
+      name: userName.value,
+      secondName: userSecondName.value,
+      phone: userPhone.value,
+      email: userEmail.value,
+      agree: dataProcessing.checked
+    }),
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      alert('Запись успешно внесена');
+    })
+    .catch((error) => {
+      alert('Запись не внесена');
+    });
+};
+
